c62be02ef53fd6c30244f774e6f2624fa8bfc252
Maza455 <same7nte@gmail.com>
2023-11-27 09:18:02 +0200

    Adding functionality

---
+++ SCRIPT/script.js
@@ -0,0 +1,12 @@
+function greet(name) {
+  console.log("Hello " + name);
+}
+
+function multiply(a, b) {
+  console.log(a * b);
+}
+
+function tableOfFive(num) {
+  let result = num * 5;
+  console.log(result);
+}
